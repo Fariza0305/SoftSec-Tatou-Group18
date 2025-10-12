4e6b74be6caf9e1610061871398da30a72687867
Fariza0305 <paryza952@gmail.com>
2025-10-12 18:59:20 +0200

    Fix: Recovered missing watermark files (metadata & qr) and updated .gitignore for database.

---
+++ server/src/watermark_metadata.py
@@ -0,0 +1,9 @@
+# decompyle3 version 3.9.3
+# Python bytecode version base 3.11 (3495)
+# Decompiled from: Python 3.11.2 (main, Apr 28 2025, 14:11:48) [GCC 12.2.0]
+# Embedded file name: /home/lab/tatou/server/src/watermark_metadata.py
+# Compiled at: 2025-10-12 04:10:45
+# Size of source mod 2**32: 101 bytes
+
+Unsupported Python version, 3.11, for decompilation
+
